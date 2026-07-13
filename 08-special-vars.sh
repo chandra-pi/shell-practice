c062adb32cbd5047d53b25659c45d4ce2c799358
chandra-pi <chandrahyd.45@gmail.com>
2026-07-13 22:33:13 +0530

    hello world started

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -4,4 +4,11 @@ echo "All variables passed to the script : $@"
 
 echo "Number of variables : $#"
 
+echo "Script Name: $0"
+echo "Current Directory: $PWD"
+echo "user running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
 
