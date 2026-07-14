7efd6e5730e6f536e2f9d0bc2816ef32b9602341
chandra-pi <chandrahyd.45@gmail.com>
2026-07-14 19:22:41 +0530

    hello colors

---
+++ 16-loops.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python3" "nginx" "httpd")
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR: Please run the script with root access $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they try to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "Installing $2 is... $G Success $N" | tee -a $LOG_FILE
+    else
+        echo -e "Installing $2 is.... $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+for package in ${PACKAGES[@]}
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed.... going to install it" | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package is already installed.... $G nothing to do $N" | tee -a $LOG_FILE
+    fi
+done
+
+
