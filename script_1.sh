d061ab4a36966cc3c769770c5194ade282d772f2
chandra-pi <chandrahyd.45@gmail.com>
2026-07-29 15:20:07 +0530

    hello shell 2

---
+++ script_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GREETING="Hello, Good Morning"
+echo "$GREETING"
+
+echo "PID od script_1: $$"
+
+#./script_2.sh
+
+source ./script_2.sh
---
+++ script_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "PID of script_2: $$"
+
+echo "$GREETING"
