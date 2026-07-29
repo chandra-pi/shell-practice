b9f1cb20c9db6bb3077aca120bde7c3ea24ff626
chandra-pi <chandrahyd.45@gmail.com>
2026-07-29 20:58:07 +0530

    hello shell 2

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,1 +1,45 @@
 #!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+#Check the user has root previliges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR: Please run the script with root access $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they try to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is... $G Success $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is.... $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executing at $(date)"
+
+SOURCE_DIR=/home/ec2-user/app-logs
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+
+while IFS= read -r filepath
+do
+    echo "Deleting file: $filepath" | tee -a $LOG_FILE
+    rm -rf $filepath
+done <<< $FILES_TO_DELETE
