cbcfea977ec39b8b4e8cb6f7cdab5530d1f3c91a
chandra-pi <chandrahyd.45@gmail.com>
2026-07-29 21:22:08 +0530

    hello shell 2

---
+++ 18-backup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+USERID=$(id -u)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if DAYA are provided that will be considered, otherwise default 14 days
+
+LOGS_FOLDER="/var/log/shellscript-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+#Check the user has root previliges or not
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR: Please run the script with root access $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "You are running with root access" | tee -a $LOG_FILE
+    fi
+}
+
+#Validate function takes input as exit status, what command they try to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is... $G Success $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is.... $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+check_root
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 18-backup.sh <source-dir> <destination-dir> <days(optional)>"
+    exit 1
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+
+
+
+
+
